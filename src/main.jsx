eb847aa4263223e6a52f1e8ed581cd2cc3773e29
Jasyti Online <JOHNBARKLE@MSN.COM>
2025-11-13 16:44:43 -0600

    docs: upload

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./theme/color.css";
-import "./index.css";
+import "./theme/index.css";
 import "./theme/overrides.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
